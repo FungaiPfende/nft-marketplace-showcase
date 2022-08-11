dbeb0768c84b7e5f8cf444c97132ee4abca5fa11
FungaiPfende <fungaip163@outlook.com>
2022-08-11 16:46:02 +0200

    Designed first section.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import { Download, Features, SectionWrapper } from "./components";
+import assets from "./assets";
 
 const App = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">
-        React Native NFT Marketplace Showcase.
-      </h1>
-      <Download />
-      <Features />
-      <SectionWrapper />
-    </div>
+    <>
+      <SectionWrapper
+        title="Your own store of Nifty NFTs. Start Selling & Growing"
+        description="Buy, store, collect NFTs, exchange & earn crypto. Join 25+ million people using ProNef Marketplace."
+        showBtn
+        mockupImg={assets.homeHero}
+        banner="banner"
+      />
+    </>
   );
 };
 
