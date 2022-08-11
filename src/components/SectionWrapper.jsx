799ec95cd2700ad65a38ff7e17d8eb7420bb5e8c
FungaiPfende <fungaip163@outlook.com>
2022-08-11 16:40:39 +0200

    Created SectionWrapper component.

--- src/components/SectionWrapper.jsx
+++ src/components/SectionWrapper.jsx
@@ -1,5 +1,40 @@
-import React from 'react'
+import React from "react";
 
-export const SectionWrapper = () => {
-  return <div>SectionWrapper</div>;
+import styles from "../styles/Global";
+import assets from "../assets";
+
+import { Button } from "./Button";
+
+export const SectionWrapper = ({
+  title,
+  description,
+  showBtn,
+  mockupImg,
+  banner,
+}) => {
+  return (
+    <div
+      className={`min-h-screen ${styles.section} ${styles.bgWhite} ${banner}`}
+    >
+      <div
+        className={`flex items-center w-11/12 sm:w-full minmd:w-3/4 ${styles.boxClass}`}
+      >
+        <div className={`fadeLeftMini ${styles.descDiv}`}>
+          <h1 className={`${styles.h1Text}`}>{title}</h1>
+          <p className={`${styles.descriptionText}`}>{description}</p>
+
+          {showBtn && (
+            <Button
+              assetUrl={assets.expo}
+              link="https://expo.dev/@kxdammo/NFTMarketplace?serviceType=classic&distribution=expo-go"
+            />
+          )}
+        </div>
+
+        <div className={`flex-1 p-8 sm:px-0 ${styles.flexCenter}`}>
+          <img src={mockupImg} alt="mockup" className={styles.sectionImg} />
+        </div>
+      </div>
+    </div>
+  );
 };
